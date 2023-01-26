de49d4c82bf16ff36126c9fbd81080201a5a85d2
ngtuan <nguyenngoctuan092@gmail.com>
2023-01-26 11:16:20 +0700

    full Canny Edge Filter update

--- modules/CannyEdgeFilter/model.py
+++ modules/CannyEdgeFilter/model.py
@@ -14,7 +14,7 @@ class CannyEdgeFilter(nn.Module):
         self.low_threshold = low_threshold
         self.high_threshold = high_threshold
         self.strong_pixel = 255
-        self.weak_pixel = 25
+        self.weak_pixel = 75
         self.sigma = sigma
 
     def forward(self, x: torch.Tensor):
@@ -34,7 +34,23 @@ class CannyEdgeFilter(nn.Module):
 if __name__ == '__main__':
 
     filter = CannyEdgeFilter(5)
+    from PIL import Image
+    import os
+    img = Image.open(os.path.join('images', 'IMG_1129.JPG'))
 
-    x = torch.rand(1, 1, 100, 100)
-    x = filter(x)
-    print(x.shape)
+    transform = transforms.Compose([
+        transforms.Grayscale(),
+        transforms.Resize((256, 256)),
+        transforms.ToTensor(),
+        transforms.GaussianBlur(5, 1)
+    ])
+
+    img = transform(img)
+    img.unsqueeze_(0)
+    img = filter(img)
+    img = img.squeeze(0)
+    img = img.squeeze(0)
+
+    import matplotlib.pyplot as plt
+    plt.imshow(img, cmap='gray')
+    plt.show()
